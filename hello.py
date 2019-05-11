42264520346fc2c7b451e4ff3d8934eb238214d9
jayesh0024 <jayesh.modhwadiya@outlook.com>
2019-05-11 12:11:33 +0530

    new code

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
 print ("hello word, how are you")
+feel = input("how is are you?")
+if feel == "good":
+    print("do your work")
+elif feel== "fine":
+    print("nice")
+else:
+    print("do something good")
